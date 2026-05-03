ca9e0a6c89ede8c1202c7ec6591cb446661d11fb
Mahesh Dara <maheshdara222gmail@gmail.com>
2026-05-03 19:45:54 +0530

    change

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/auth", require("./routes/auth"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`);
+// });
